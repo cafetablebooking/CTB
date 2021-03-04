5b8e98bc7510854141efea245c4d1aa48937dc8d
g-holmer <gabbeholmer@hotmail.com>
2021-03-04 13:31:37 +0100

    Fix import

--- apps/client/pages/connectBusiness.tsx
+++ apps/client/pages/connectBusiness.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Typography, Box, TextField, Button, Divider } from '@material-ui/core';
 
 import { ThemeProvider } from '@material-ui/core/styles';
-import { darkTheme } from '../components/ThemeProviders/DarkThemeProvider';
+import darkTheme from '../components/ThemeProviders/DarkThemeProvider';
 import Image from 'next/image';
 
 import { useForm } from 'react-hook-form';
