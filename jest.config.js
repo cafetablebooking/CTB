6e8f227fda0815727b5d0410926962a64ab0ff5b
g-holmer <gabbeholmer@hotmail.com>
2021-03-08 13:17:06 +0100

    Fix merge conflict for admin

--- jest.config.js
+++ jest.config.js
@@ -4,15 +4,12 @@ module.exports = {
     '<rootDir>/apps/client',
     '<rootDir>/libs/firebase-auth',
     '<rootDir>/libs/auth-context',
-<<<<<<< HEAD
     '<rootDir>/libs/auth-crud',
     '<rootDir>/libs/yup-resolvers',
-=======
     '<rootDir>/libs/types',
     '<rootDir>/apps/test-admin',
     '<rootDir>/apps/functions',
     '<rootDir>/apps/cloud-functions',
     '<rootDir>/libs/use-local-storage',
->>>>>>> feature/owner-dashboard-setup
   ],
 };
