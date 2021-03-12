4458c34a07019d361830a07314779b6b7bb43551
g-holmer <gabbeholmer@hotmail.com>
2021-03-12 09:35:48 +0100

    cleanup dialogboxcomp

--- apps/client/pages/company/ConfirmDialogBoxComponent.tsx
+++ apps/client/pages/company/ConfirmDialogBoxComponent.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 
 import DialogTitle from '@material-ui/core/DialogTitle';
 import Dialog from '@material-ui/core/Dialog';
 
 import Typography from '@material-ui/core/Typography';
-import { blue } from '@material-ui/core/colors';
+
 import {
   Box,
   Button,
@@ -22,14 +21,8 @@ export interface SimpleDialogProps {
   bookedInfo: any;
   tableBookings: any;
 }
-const useStyles = makeStyles({
-  avatar: {
-    backgroundColor: blue[100],
-    color: blue[600],
-  },
-});
+
 function SimpleDialog(props: SimpleDialogProps) {
-  const router = useRouter();
   const {
     onClose,
     selectedValue,
@@ -49,12 +42,6 @@ function SimpleDialog(props: SimpleDialogProps) {
 
   const handleClose = () => {
     onClose(selectedValue);
-  };
-  const handleListItemClick = (value: string) => {
-    onClose(value);
-  };
-  const handleRedirect = () => {
-    router.push('/payment');
   };
 
   return (
