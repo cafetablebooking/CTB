1bd5f403b5b0224c663bbf06a13cb9df1810f25a
g-holmer <gabbeholmer@hotmail.com>
2021-03-16 12:06:18 +0100

    Update PaymentDialogComponent.tsx

--- apps/client/pages/company/PaymentDialogComponent.tsx
+++ apps/client/pages/company/PaymentDialogComponent.tsx
@@ -90,7 +90,7 @@ function PaymentDialog(props: PaymentDialogProps) {
     if (!error) {
       try {
         const { id } = paymentMethod;
-        const url = 'http://localhost:4000/payment';
+        const url = 'https://stripe-server-ctb.herokuapp.com/payment:4000';
         const response = await axios.post(url, {
           amount: 100,
           id,
