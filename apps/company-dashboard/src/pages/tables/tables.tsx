93d480899f480b7512a81a50a80c8c4b25021ad7
g-holmer <gabbeholmer@hotmail.com>
2021-04-05 14:30:48 +0200

    Delete unused files and add resource logic

--- apps/company-dashboard/src/pages/tables/tables.tsx
+++ apps/company-dashboard/src/pages/tables/tables.tsx
@@ -7,22 +7,22 @@ import { useFirestore } from '@ctb/use-firestore';
 import CustomToolBarSelect from './CustomToolBarSelect';
 import { firestore } from '@ctb/firebase-auth';
 import { useAuthContext } from '@ctb/auth-context';
-import DialogBox from './DialogBox/DialogBox';
+import { DialogBox } from '@ctb/alert-dialog-box';
 /* eslint-disable-next-line */
 export interface UsersProps {}
 // export interface option {}
 const columns = [
   {
-    name: 'companyName',
-    label: 'Name',
+    name: 'resourceId',
+    label: 'Id',
     options: {
       filter: true,
       sort: true,
     },
   },
   {
-    name: 'email',
-    label: 'Email',
+    name: 'resourceTitle',
+    label: 'Name',
     options: {
       filter: true,
       sort: true,
@@ -56,7 +56,14 @@ export function Tables(props: UsersProps) {
     setOpen(false);
   };
 
-  const { docs } = useFirestore('company_requests');
+  const { docs } = useFirestore('tableBookings');
+
+  const table =
+    docs &&
+    docs.find((item) => {
+      return item.companyId === '3c672850-82bf-11eb-8ed2-9d3621fb18a8';
+    });
+  const resources = table && table.resources;
 
   const { signup }: any = useAuthContext();
 
@@ -66,7 +73,7 @@ export function Tables(props: UsersProps) {
 
       const { id } = company;
       if (!id) return;
-      const tables = firestore.collection('tables').doc(id);
+      const tables = firestore.collection('tableBookings').doc(id);
       await tables.delete();
       handleClose();
     });
@@ -96,8 +103,8 @@ export function Tables(props: UsersProps) {
         actionType={actionType}
       />
       <MUIDataTable
-        title={'Pending Companies list'}
-        data={docs}
+        title={'Table list'}
+        data={resources}
         columns={columns}
         options={options}
       />
